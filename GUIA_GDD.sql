de345073291977a0da068e79cb805044b82173be
Ignacio Crusoe <icrusoe@frba.utn.edu.ar>
2023-09-14 12:39:40 -0300

    Ejercicios 3 y 4

--- GUIA_GDD.sql
+++ GUIA_GDD.sql
@@ -9,4 +9,28 @@ ORDER BY clie_codigo;
 
 /*EJERCICIO 2*/
 /*Mostrar el código, detalle de todos los artículos vendidos en el año 2012 ordenados por
-cantidad vendida.*/SELECT prod_codigo, prod_detalle FROM ProductoJOIN Item_Factura ON prod_codigo = item_productoJOIN Factura ON fact_tipo+fact_sucursal+fact_numero = item_tipo+item_sucursal+item_numeroWHERE year(fact_fecha) = 2012GROUP BY prod_codigo, prod_detalleORDER BY sum(item_cantidad);
+cantidad vendida.*/
+SELECT prod_codigo, prod_detalle FROM Producto
+JOIN Item_Factura ON prod_codigo = item_producto
+JOIN Factura ON fact_tipo+fact_sucursal+fact_numero = item_tipo+item_sucursal+item_numero
+WHERE year(fact_fecha) = 2012
+GROUP BY prod_codigo, prod_detalle
+ORDER BY sum(item_cantidad);
+
+/*EJERCICIO 3*/
+/*Realizar una consulta que muestre código de producto, nombre de producto y el stock
+total, sin importar en que deposito se encuentre, los datos deben ser ordenados por
+nombre del artículo de menor a mayor*/
+SELECT prod_codigo, prod_nombre, SUM(ISNULL(stoc_cantidad, 0)) FROM roducto LEFT JOIN STOCK ON prod_codigo = stoc_producto
+GROUP BY prod_codigo, prod_detalle
+ORDER BY prod_deralle;
+
+/*EJERCICIO 4*/
+/*Realizar una consulta que muestre para todos los artículos código, detalle y cantidad de
+artículos que lo componen. Mostrar solo aquellos artículos para los cuales el stock
+promedio por depósito sea mayor a 100.*/
+SELECT prod_codigo, prod_detalle, count(comp_componente)
+FROM Producto LEFT JOIN Composicion ON  prod_codigo = comp_producto
+WHERE prod_codigo IN (SELECT prod_codigo FROM STOCK GROUP BY stoc_producto HAVING AVG(stoc_cantidad) > 100)
+GROUP BY prod_codigo, prod_detalle
+ORDER BY COUNT(comp_componente) DESC
